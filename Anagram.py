5d80615aa7feb3d1e811b7a34c55ebaa631c7532
Marina Miyamoto <m.miyamoto0320@gmail.com>
2019-05-27 02:49:27 +0900

    Anagram Solving Program

--- Anagram.py
+++ Anagram.py
@@ -12,13 +12,27 @@ bs = BeautifulSoup(response.content, "lxml")
 dictionary = bs.select('p')[0].text
 b = dictionary.lower().split('\n')
 
+list = []
 new_list = []
 for i in range(len(b)):
     d = sorted(b[i])
-    
-    if a == d:
-        new_list.append(b[i])
+    list.append(d)
 
+#a is the words that was typed in
+#d is the each words that are in the dictionary 
+for x in range(len(list)):
+        a = sorted(words)
+        for y in range(len(list[x])):
+                if list[x][y] in a:
+                        a.remove(list[x][y])
+                        flag = True
+                        
+                else:
+                        flag = False
+                        break
+        if flag == True:
+                new_list.append(b[x])
 
-for h in range(len(new_list)):
-    print(new_list[h])
+for z in range(len(new_list)):
+        M = max(new_list, key = len)
+print(M)
