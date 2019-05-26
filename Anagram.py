d4625f350e335bf23316231d02272f1626aa5978
Marina Miyamoto <m.miyamoto0320@gmail.com>
2019-05-27 02:37:26 +0900

    Anagram with Less Letters

--- Anagram.py
+++ Anagram.py
@@ -15,21 +15,24 @@ b = dictionary.lower().split('\n')
 list = []
 new_list = []
 for i in range(len(b)):
-        d = sorted(b[i])
-        list.append(d)
+    d = sorted(b[i])
+    list.append(d)
 
 #a is the words that was typed in
 #d is the each words that are in the dictionary 
 for x in range(len(list)):
+        a = sorted(words)
         for y in range(len(list[x])):
                 if list[x][y] in a:
+                        a.remove(list[x][y])
                         flag = True
+                        
                 else:
                         flag = False
                         break
         if flag == True:
                 new_list.append(b[x])
-        
+
 #for h in range(len(new_list)):
 #    print(new_list[h])
-print(new_list)
+print(new_list) 
