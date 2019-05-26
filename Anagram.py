036d48de4833e48946b559946031ac6282c6ebbb
Marina Miyamoto <m.miyamoto0320@gmail.com>
2019-05-27 01:37:30 +0900

    Anagram with Less Letters

--- Anagram.py
+++ Anagram.py
@@ -12,13 +12,24 @@ bs = BeautifulSoup(response.content, "lxml")
 dictionary = bs.select('p')[0].text
 b = dictionary.lower().split('\n')
 
+list = []
 new_list = []
 for i in range(len(b)):
-    d = sorted(b[i])
-    
-    if a == d:
-        new_list.append(b[i])
+        d = sorted(b[i])
+        list.append(d)
 
-
-for h in range(len(new_list)):
-    print(new_list[h])
+#a is the words that was typed in
+#d is the each words that are in the dictionary 
+for x in range(len(list)):
+        for y in range(len(list[x])):
+                if list[x][y] in a:
+                        flag = True
+                else:
+                        flag = False
+                        break
+        if flag == True:
+                new_list.append(b[x])
+        
+#for h in range(len(new_list)):
+#    print(new_list[h])
+print(new_list)
