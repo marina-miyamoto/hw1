eecce7e31b8a411c55cf106f811670591f21aafc
Marina Miyamoto <m.miyamoto0320@gmail.com>
2019-05-30 16:41:30 +0900

    Refined the Program According to Feedbacks

--- Anagram.py
+++ Anagram.py
@@ -1,16 +1,14 @@
-import requests
-from bs4 import BeautifulSoup
-
 characters = input("Type in the words: ")
 characters = characters.replace(' ', '')
-sorted_input = sorted(characters)
-print("The letters entered were: " + str(sorted_input))
-
 
 with open("sample1.csv", "r") as f:
     dictionary = f.readlines()
 b = dictionary[0].lower().split(',')
 
+'''
+with open("dictionary.words", "r") as f:
+        dictionary = [w.lower().strip("\n") for w in f.readlines()]
+'''
 
 dictionary_words = []
 anagram_words = []
@@ -24,40 +22,35 @@ two_points = ['c', 'f', 'h', 'l', 'm', 'p', 'v','w', 'y']
 three_points = ['j', 'k', 'q', 'x', 'z']
 
 
-for x in range(len(dictionary_words)):
+for word in dictionary_words:
         sorted_input = sorted(characters)
-        for y in range(len(dictionary_words[x])):
-                if dictionary_words[x][y] in sorted_input:
+        for char in word:
+                if char in sorted_input:
                         #removing a letter to avoid words with same two letters
-                        sorted_input.remove(dictionary_words[x][y])
+                        sorted_input.remove(char)
                         flag = True
                 
                 else:
                         flag = False
                         break
                 
-
         if flag == True:
-                anagram_words.append(b[x])
+                i = dictionary_words.index(word)
+                anagram_words.append(b[i])
 
-for x in range(len(anagram_words)):
+
+scored_anagram = []
+for anagram_word in anagram_words:
         score = 0
-        for y in range(len(anagram_words[x])):
-                if anagram_words[x][y] in two_points:
+        for x in anagram_word:
+                if x in two_points:
                         score += 2
-                elif anagram_words[x][y] in three_points:
-                        score += 3
-                else: score += 1
-        scores.append(score)
-                                
-                
-longest_anagram = max(anagram_words, key = len)
-anagram_index = anagram_words.index(longest_anagram)
-highest_score = scores.index(max(scores))
+                elif x in three_points:
+                        score+= 3
+                else:
+                        score += 1
+        scored_anagram.append((anagram_word, score))
 
-if scores[anagram_index] >= scores[highest_score]:
-        print('Highest Score: ' + str((scores[anagram_index]+1)**2))
-        print(anagram_words[anagram_index])
-else:
-        print('Highest Score: ' + str((scores[highest_score]+1)**2))
-        print(highest_score)
+highest_score = max(scored_anagram, key = lambda v: v[1])[0]
+print("Highest Scoring Anagram: " + highest_score)
+                
